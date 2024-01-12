14f60f78ce13cc71e23f320e17b28aa0deb7c179
Willyan T. Ferdinando <willyantferdinando@gmail.com>
2024-01-12 14:31:31 -0300

    Estrutura Inicial SpringBoot

---
+++ src/test/java/io/wferdinando/payment/system/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wferdinando.payment.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
